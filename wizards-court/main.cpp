ac2d813305406b4876fbf512c0b56388fc93662d
Daniel Fuller <lasaldan@gmail.com.com>
2015-09-12 22:06:17 -0600

    Initial Commit

---
+++ wizards-court/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  wizards-court
+//
+//  Created by Daniel Fuller on 9/12/15.
+//  Copyright (c) 2015 Daniel Fuller. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
